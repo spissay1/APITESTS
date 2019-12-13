05cc887ba4540585afd860ecb764c1e02d0a844d
shwetha <shwethapissay@gmail.com>
2019-12-13 13:23:27 +0530

    add item to cart and delete automation script added

---
+++ src/test/java/AddDeleteItems.java
@@ -0,0 +1,84 @@
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import javax.print.attribute.HashPrintJobAttributeSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class AddDeleteItems {
+
+    public static String authToken;
+
+    @BeforeClass
+    public void authToken() {
+        Response res = given()
+                .formParam("grant_type","password")
+                .formParam("username", "shwethapissay@gmail.com")
+                .formParam("password", "pass123")
+                .post("https://spree-vapasi-prod.herokuapp.com/spree_oauth/token");
+
+        authToken = "Bearer " + res.path("access_token");
+    }
+
+    @Test (priority = 0)
+    public void getProducts() {
+        Response res = given()
+                .get("https://spree-vapasi-prod.herokuapp.com/api/v2/storefront/products");
+        System.out.println(res.jsonPath().prettify());
+
+    }
+
+    @Test (priority = 1)
+    public void testAddItem(){
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("Content-Type", "application/json");
+        headers.put("Authorization", authToken);
+
+        String createBody = "{\n" +
+                "  \"variant_id\": \"17\",\n" +
+                "  \"quantity\": 5\n" +
+                "}";
+
+        Response res = given()
+                .headers(headers)
+                .body(createBody)
+                .when()
+                .post("https://spree-vapasi-prod.herokuapp.com/api/v2/storefront/cart/add_item");
+
+        Assert.assertEquals(res.statusCode(), 200);
+    }
+
+    @Test (priority = 2)
+    public void viewCart() {
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("Content-Type", "application/json");
+        headers.put("Authorization", authToken);
+
+        Response res = given()
+                .headers(headers)
+                .get("https://spree-vapasi-prod.herokuapp.com/api/v2/storefront/cart");
+        System.out.println(res.jsonPath().prettify());
+
+    }
+
+    @Test (priority = 3)
+    public void deleteItem(){
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("Content-Type", "application/json");
+        headers.put("Authorization", authToken);
+        Response res = given()
+                .headers(headers)
+                .when()
+                .delete("https://spree-vapasi-prod.herokuapp.com/api/v2/storefront/cart/remove_line_item/423");
+        System.out.println(res.jsonPath().prettify());
+
+    }
+}
--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -1,7 +1,10 @@
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import javax.print.attribute.HashPrintJobAttributeSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +14,8 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class BasicTest {
+
+    public static String authToken;
 
     @Test(priority = 0)
     public void testStatusCode() {
@@ -50,7 +55,78 @@ public class BasicTest {
         {
             Map attributes = (Map)product.get("attributes");
             System.out.println("Product ID::"+ product.get("id") + " Currency::" + attributes.get("currency"));
+            Assert.assertEquals(attributes.get("currency"), "USD");
+            //Assert.assertTrue(attributes.get(("currency")).equals("USD");
         }
 
     }
+    @Test(priority = 4)
+    public void testFilter() {
+        Response res = given()
+                .log().all()
+                .queryParam("filter[name]", "bag")
+                .get("https://spree-vapasi-prod.herokuapp.com/api/v2/storefront/products");
+
+          System.out.println(res.prettyPrint());
+
+    }
+
+    @Test(priority = 5)
+    public void testFilterIds() {
+        Response res = given()
+                .log().all()
+                .queryParam("filter[ids]", "1")
+                .get("https://spree-vapasi-prod.herokuapp.com/api/v2/storefront/products");
+
+        System.out.println(res.prettyPrint());
+
+    }
+    @Test(priority = 6)
+    public void testFilterPrice() {
+        Response res = given()
+                .log().all()
+                .queryParam("filter[price]", "19.99")
+                .get("https://spree-vapasi-prod.herokuapp.com/api/v2/storefront/products");
+        System.out.println(res.prettyPrint());
+    }
+
+    @Test(priority = 7)
+    public void testFilterOptionsColour() {
+        Response res = given()
+                .log().all()
+                .queryParam("filter[options][tshirt-color]", "S","Red")
+                .get("https://spree-vapasi-prod.herokuapp.com/api/v2/storefront/products");
+        System.out.println(res.prettyPrint());
+    }
+    @BeforeClass
+    public void authToken() {
+        Response res = given()
+                .formParam("grant_type","password")
+                .formParam("username", "shwethapissay@gmail.com")
+                .formParam("password", "pass123")
+                .post("https://spree-vapasi-prod.herokuapp.com/spree_oauth/token");
+
+        authToken = "Bearer " + res.path("access_token");
+    }
+
+    @Test
+    public void testPostCall(){
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("Content-Type", "application/json");
+        headers.put("Authorization", authToken);
+
+        String createBody = "{\n" +
+                "  \"variant_id\": \"17\",\n" +
+                "  \"quantity\": 5\n" +
+                "}";
+
+        Response res = given()
+                    .headers(headers)
+                    .body(createBody)
+                    .when()
+                    .post("https://spree-vapasi-prod.herokuapp.com/api/v2/storefront/cart/add_item");
+
+        Assert.assertEquals(res.statusCode(), 200);
+    }
+
 }
